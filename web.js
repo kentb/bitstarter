958d8a975fceb9d250ae8b762efa2f01be0ae4c4
Kent Buenaventura <kent.buenaventura@yahoo.com>
2013-07-12 00:00:55 -0700

    Read index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = fs.readFileSync("index.html");
+  response.send(buf.toString('utf8', 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
